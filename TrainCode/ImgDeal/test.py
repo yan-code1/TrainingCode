eb597cc002d60113ce35009a9e2d5607dcb2efac
unknown <1977435273@qq.com>
2021-04-20 15:56:54 +0800

    fill

--- TrainCode/ImgDeal/test.py
+++ TrainCode/ImgDeal/test.py
@@ -29,4 +29,32 @@ import random
 # cv.putText(img,text,(0,img.shape[0]-5),cv.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
 # cv.imshow('image',img)
 # cv.waitKey(0)
+import cv2
+import numpy as np
+import os
 
+def fillBlack(img):
+     # pixel = np.zeros(3)
+     pixel = 0
+     for i in range(img.shape[0]):
+          for j in range(img.shape[1]):
+               if img[i][j] != 0:
+                    pixel = img[i][j]
+
+          img[][]
+for path in open("1.txt"):
+     path = path.replace('\n', '')  # 去除换行符号
+     print(path)
+     img = cv2.imread(path, 1)
+
+     import  time
+     s = time.time()
+     width, height = img.shape[:2][::-1]
+     img_resize = cv2.resize(img, (int(width * 1.0), int(height * 1.0)), interpolation = cv2.INTER_CUBIC)
+     img_gray = cv2.cvtColor(img_resize, cv2.COLOR_RGB2GRAY)
+     img_temp = img.flatten()
+     # img_gray = img_gray[img_gray.shape[0]-10:img_gray.shape[0],img_gray.shape[1]-10:img_gray.shape[1]]
+     imageVar = cv2.Laplacian(img_gray, cv2.CV_64F).var()  # 图像模糊度
+     # print('>>>', path)
+     # cv2.imshow(img_gray)
+     print(">>>", int(imageVar),time.time() - s)
